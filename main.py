cae0562e9b944b4e007f7775e86e71fea8a875cc
AndreyTheHacker <andrejpavlenko666@gmail.com>
2021-07-09 21:35:47 +0900

    Added Palette support & compact

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import png
 import os
 import sys
 end = "\x1b[0m"
+lowerhb = "\u2584"
+higherhb = "\u2580"
 br = 0
 oldstyle=""
-term = os.popen("stty size","r").read().split(" ")
+[term_h,term_w] = os.popen("stty size","r").read().split(" ");
+[term_h,term_w] = [int(term_h),int(term_w)]
 if len(sys.argv)==4:
     normarg = sys.argv[-3]
     br = int(sys.argv[-2])
@@ -24,19 +27,34 @@ rd = png.Reader(normarg)
 width, height, data, other = rd.read_flat()
 isalpha = other['alpha']
 print("%sx%s"%(width,height))
-pw = 4
-x, y = (0,0)
-trig = False
-while y<height:
-    x=0
-    while x<width:
-        p=data[(x+(y*width))*4:((x+(y*width))+1)*4] if isalpha==True else data[(x+(y*width))*3:((x+(y*width))+1)*3]
-        #print(p)
-        r,g,b=(p[0],p[1],p[2]) if len(p)>=3 else (0,0,0)
-        draw = "\x1b[38;2;%s;%s;%sm"%(r+br if r+br>0 else 0,g+br if g+br>0 else 0,b+br if b+br>0 else 0)
-        x+=1
-        print("%s\u2588%s"%(draw,end),end='') if len(oldstyle)<=0 else print("%s#%s"%(draw,end),end='')
-        
-    print("")
-    y+=1
- 
+scr_w = (width/term_w)
+scr_h = (height/term_h)
+scr = 1 # SCaling Ratio
+plt = ('palette' in other, other['palette'] if 'palette' in other else ())
+th = int(height/scr)
+tw = int(width/scr)
+def DrawImage(data,tw,th,plt):
+    x, y = (0,0)
+    while y<th/2:
+        x=0
+        while x<tw:
+            nx = scr*x
+            ny = int(scr*y)*2
+            nx1 = (nx+(ny*width))
+            nx2 = (nx+((ny-1)*width))
+            if plt[0]==False:
+                p=data[nx1*4:(nx1+1)*4] if isalpha==True else data[nx1*3:(nx1+1)*3]
+                q=data[nx2*4:(nx2+1)*4] if isalpha==True else data[nx2*3:(nx2+1)*3]
+            else:
+                p=plt[1][data[nx1]]
+                q=plt[1][data[nx2]]
+            r,g,b=(p[0],p[1],p[2]) if len(p)>=3 else (0,0,0)
+            r1,g1,b1=(q[0],q[1],q[2]) if len(q)>=3 else (0,0,0)
+            draw = "\x1b[48;2;%s;%s;%sm"%(r+br if r+br>0 else 0,g+br if g+br>0 else 0,b+br if b+br>0 else 0)
+            draw1 = "\x1b[38;2;%s;%s;%sm"%(r1+br if r1+br>0 else 0,g1+br if g1+br>0 else 0,b1+br if b1+br>0 else 0)
+            x+=1
+            #print("%s\u2588%s"%(draw,end),end='') if len(oldstyle)<=0 else print("%s#%s"%(draw,end),end='')
+            print("%s%s%s%s"%(draw,draw1,higherhb,end),end='',flush=True) if len(oldstyle)<=0 else print("%s#%s"%(draw,end),end='')
+        print("")
+        y+=1
+DrawImage(data,tw,th,plt)
